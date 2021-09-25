7d2c536da9a26179693c26a991ee5440fb533e5e
unknown <kabeyenejosianeprisca@gmail.com>
2021-09-25 17:00:24 +0100

    page d'accueil

---
+++ script.js
@@ -0,0 +1,88 @@
+
+
+
+    
+  $('.populaire').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
+
+    
+  $('.reputer').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
+
+
+ $('.contenant4').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.contenant2',
+  dots: true,
+  centerMode: true,
+  focusOnSelect: true
+});
+
+$('.contenant3').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
+
+
+$('.contenant2').slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear'
+});
+  
+    
+  $('.proche').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
+
+
+$('.cadremin1').slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear'
+});
+$('.cadremin2').slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear'
+});
+
+$('.cadremin1').slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear'
+});
+
+
+$('.cadremin2').slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear'
+});
+
+
+   
