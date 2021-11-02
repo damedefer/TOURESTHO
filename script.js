c245a4cd0413b914e86ba34b9c1f83029f358dd6
unknown <kabeyenejosianeprisca@gmail.com>
2021-11-02 22:20:21 +0100

    modification3

--- script.js
+++ script.js
@@ -1,34 +1,5 @@
 /* baniere_----------------------------------------------------------------------------------_____*/
 
-var video1 = document.getElementById('video1'); 
-var video2 = document.getElementById('video2'); 
-var video3 = document.getElementById('video3');
-var text = document.getElementById('text');
-
-
-
-
-video1.onended = function(){
-  video2.play();
-  video1.style.opacity=0;
-   video2.style.opacity=1;
-
-   text.innerHTML=" N'hesitez plus lancez vous, nous vous dirigeons et accompagnons dans tout vos projets de voyage vers le cameroun";
-}
-video2.onended = function(){
-  video3.play();
-  video2.style.opacity=0;
-  video3.style.opacity=1;
-  text.innerHTML='  Venez Visitez notre faune et vous y verrez des merveilles';
-  
-}
-video3.onended = function(){
-  video1.play();
-  video3.style.opacity=0;
-  video1.style.opacity=1;
-   text.innerHTML='Sortez et faites des rencontres dans les meilleures restaurants des villes de yaounde et douala';
-
- }
  $('.resto-slide').slick({
   dots: true,
   infinite: true,
